ba40c806c321b0e23f1c73ec52b24d68dc445375
GulruhIsmayil <111437145+GulruhIsmayilova@users.noreply.github.com>
2023-05-09 13:29:18 +0400

    first commit

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,38 @@
-let a=5;
-let b=6;
-a=a+b; // 11
-b=a-b;  //5
-a=a-b;  //6
-console.log(a,b)
-//console.loga a,b yazdiqda 5,6 evezine 6,5 verir
 
-let c= "salam";
-let d=c.split("").reverse().join("").toString();
-console.log(  d)
+//JavaScript String Methods
 
-//"salam" stringinin hərflerini tersine deyişir.
+// 1.length:()
+// let myString = "Hello, world!";
+//console.log(myString.length); // Output: 13
+
+// 2.toUpperCase():
+//let myString = "Hello, Anka!";
+//console.log(myString.toUpperCase()); // Output: HELLO, WORLD!
+
+//3.toLowerCase(): 
+//let myString = "Hello, world!";
+//console.log(myString.toLowerCase()); // Output: hello, world!
+
+//4.indexOf(): 
+//let myString = "Hello, world!";
+//console.log(myString.indexOf("world")); // Output: 7
+//console.log(myString.indexOf("l")); // Output: 2
+
+//5.slice(): 
+//let myString = "Hello, world!";
+//console.log(myString.slice(0, 5)); // Output: Hello
+//console.log(myString.slice(7)); // Output: world!
+
+//6.replace(): 
+//let myString = "Hello, world!";
+//console.log(myString.replace("world", "Anka")); // Output: Hello, everyone!
+
+//7. concat(): 
+let str1 = "Hello";
+let str2 = "world!";
+let str3 =  "Anka!";
+console.log(str1.concat(", ", str2,str3)); // Output: Hello, world!
+
+//8.trim():
+//let myString = "   Hello, world!   ";
+//console.log(myString.trim()); // Output: Hello, world!
